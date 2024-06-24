cb60ca41d138defa7b98b9f7e6ed653959403727
josie <josietran.03@gmail.com>
2024-06-24 16:34:40 -0700

    changed swiping icon + ccleaned up code

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,5 @@
 let slideChangeCount = 0;
 const swiper = new Swiper('.mySwiper', {
-  // Optional parameters
   slidesperview: 4,
   direction: 'horizontal',
   loop: false,
@@ -10,52 +9,24 @@ const swiper = new Swiper('.mySwiper', {
     enabled: true,
     onlyInViewport: false,
   },
-  // setWrapperSize: true,
-  // slidesOffsetAfter: 2,
-  // zoom: {
-  //   maxRatio: 100,
-  // },
 
-  // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  //   clickable: true,
-  // },
-
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
 
   speed: 500,
   spaceBetween: 20,
 
   on: {    
     init: function () {      
-    checkNavigationButtons(); // Check button state on initialization    
+    checkNavigationButtons(); 
   },    
   slideChange: function () {      
-    checkNavigationButtons(); // Check button state on slide change    
+    checkNavigationButtons();     
 },  },
   
 });
-
-// const nextButton = document.querySelector('.next-and-prev-buttons .next');
-//     const prevButton = document.querySelector('.next-and-prev-buttons .prev');
-
-//     nextButton.addEventListener('click', function () {
-//       swiper.slideNext();
-//     });
-
-//     prevButton.addEventListener('click', function () {
-//       swiper.slidePrev();
-//     });
 
  const swiperTest = document.querySelector('.mySwiper').swiper;
 
@@ -64,6 +35,3 @@ if (swiper.isEnd) {
   nextButton.classList.add('disabled');
   nextButton.disabled = true;
 }
-
-// Now you can use all slider methods like
-// swiperTest.slideNext();
