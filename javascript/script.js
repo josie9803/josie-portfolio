d912cfdb2afc73531db22498b16e45b73bacd729
josie <josietran.03@gmail.com>
2024-06-25 00:20:47 -0700

    added modal for exp details

--- javascript/script.js
+++ javascript/script.js
@@ -17,14 +17,6 @@ const swiper = new Swiper('.mySwiper', {
 
   speed: 500,
   spaceBetween: 20,
-
-  on: {    
-    init: function () {      
-    checkNavigationButtons(); 
-  },    
-  slideChange: function () {      
-    checkNavigationButtons();     
-},  },
   
 });
 
@@ -35,3 +27,25 @@ if (swiper.isEnd) {
   nextButton.classList.add('disabled');
   nextButton.disabled = true;
 }
+// for modal boxes
+var modal_wics = document.getElementById("modal-wics");
+var btn_wics = document.querySelector(".view-details-btn");
+var closeButton = document.querySelector(".close-button");
+
+btn_wics.onclick = function() {
+  modal_wics.style.display = "block";
+  document.body.classList.add("modal-open"); 
+}
+
+closeButton.onclick = function() {
+  modal_wics.style.display = "none";
+  document.body.classList.remove("modal-open");
+}
+
+window.onclick = function(event) {
+  if (event.target == modal_wics) {
+    modal_wics.style.display = "none";
+    document.body.classList.remove("modal-open"); 
+  }
+}
+
