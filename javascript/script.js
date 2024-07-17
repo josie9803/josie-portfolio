63d8408b4f72c868454fcbe33cb3605fc5a66233
josie <josietran.03@gmail.com>
2024-07-17 15:19:03 -0700

    added content for #projects, still need responsive design

--- javascript/script.js
+++ javascript/script.js
@@ -214,3 +214,59 @@ document.getElementById('rateMeForm').addEventListener('submit', function(event)
     alert('Please select a rating.');
   }
 });
+
+//hovering video card
+const card = document.querySelector('.item3');
+const video = document.querySelector('.video');
+
+card.addEventListener('mouseover', () => {
+  video.play();
+})
+
+card.addEventListener('mouseleave', () => {
+  video.pause();
+})
+
+//toggle theme button
+// check for saved 'darkMode' in localStorage
+let darkMode = localStorage.getItem('darkMode'); 
+
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+const enableDarkMode = () => {
+  // 1. Add the class to the body
+  document.body.classList.add('darkmode');
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove('darkmode');
+  // 2. Update darkMode in localStorage 
+  localStorage.setItem('darkMode', null);
+}
+ 
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+  // get their darkMode setting
+  darkMode = localStorage.getItem('darkMode'); 
+  
+  // if it not current enabled, enable it
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+  // if it has been enabled, turn it off  
+  } else {  
+    disableDarkMode(); 
+  }
+});
+
+
+
+
